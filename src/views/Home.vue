96acf417111c42f079574ebcce86651e5303d71e
alifma <alifmaulana26@gmail.com>
2021-02-16 11:01:34 +0700

    Git Init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container pt-5">
+  <div class="card">
+    <div class="card-header text-center">
+      <h1>Aplikasi Chatting</h1>
+    </div>
+    <div class="card-body">
+      <p v-for="(item, index) in content " :key="index"> <b>{{item.username}}:</b> {{item.text}}</p>
+    </div>
+    <div class="card-footer">
+        <form action="" @submit.prevent="setPesan()" class="form-inline text-center">
+          <div class="text-center w-100">
+            <h5>Logged in as {{username}}</h5>
+            <input v-model="text" type="text" class="form-control mr-2" placeholder="Write Something">
+            <button class="btn btn-warning font-weight-bold">Send</button>
+          </div>
+        </form>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import io from 'socket.io-client'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      socket: io('http://localhost:4000'),
+      text: '',
+      username: this.$route.query.username,
+      content: []
+    }
+  },
+  methods: {
+    test () {
+      this.socket.emit('test', 'hello World')
+    },
+    setPesan () {
+      const data = {
+        text: this.text,
+        username: this.username
+      }
+      this.socket.emit('pesan', data)
+      this.text = ''
+    },
+    getPesan () {
+      this.socket.on('response-pesan', (data) => {
+        console.log(data)
+        this.content = [...this.content, data]
+      })
+    }
+  },
+  mounted () {
+    this.test()
+    this.getPesan()
   }
 }
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container pt-5">
+    <div class="card">
+      <div class="card-header text-center">
+        <h1>Halaman Login</h1>
+      </div>
+      <div class="card-body">
+        <form @submit.prevent ="onLogin()" action="" class="text-center">
+          <label class="font-weight-bold" for="">Username </label>
+          <input required v-model="username" type="text" class="form-control my-2">
+          <button class="btn btn-warning font-weight-bold">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    onLogin () {
+      this.$router.push(`/home?username=${this.username}`)
+    }
+  }
+}
+</script>
